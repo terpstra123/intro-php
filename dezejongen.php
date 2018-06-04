b6728a135424e51b8e2bde89c1a04143326c3953
bjorn terpstra <bjornterpstra@hotmail.com>
2018-06-04 09:34:44 +0200

    Add files via upload

---
+++ dezejongen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>070 php variabelen weergeven</h1>
+    </div>
+    <p><?php $aantal = "14";
+        $dezejongen = "deze jongen is ";
+        echo $dezejongen.$aantal ?></p>
+</div>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
